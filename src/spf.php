7d2d8b26273a67d3c10502586fdcdcaaaacf99bd
Simon Downes <simon@eurogamer.net>
2013-02-06 12:21:40 +0000

    More utility functions

--- src/spf.php
+++ src/spf.php
@@ -72,7 +72,6 @@ function d( $var, $echo = true, $max_depth = 4, $depth = 0 ) {
  *
  * @param  mixed   $var   The variable to be dumped
  * @return
- *
  */
 function dd( $var ) {
 	if( !SPF_CLI )
@@ -86,7 +85,6 @@ function dd( $var ) {
  *
  * @param  mixed   $var
  * @return string
- *
  */
 function var_info( $var ) {
 	if( is_null($var) ) {
@@ -113,13 +111,83 @@ function var_info( $var ) {
 }
 
 /**
+ * Converts a string representation containing one or more of hours, minutes and seconds into a total number of seconds.
+ * e.g. seconds("3 hours 4 minutes 10 seconds"), seconds("5min"), seconds("4.5h")
+ *
+ * @param  string  $str   string to convert
+ * @return integer|float
+ */
+function seconds( $str ) {
+	
+	$hours   = 0;
+	$minutes = 0;
+	$seconds = 0;
+	
+	if( preg_match('/^\d+:\d+$/', $str) ) {
+		list(, $minutes, $seconds) = explode(':', $str);
+	}
+	elseif( preg_match('/^\d+:\d+:\d+$/', $str) ) {
+		list($hours, $minutes, $seconds) = explode(':', $str);
+	}
+	else {
+		
+		// convert invalid characters to spaces
+		$str = preg_replace('/[^a-z0-9. ]+/i', ' ', $str);
+		
+		// strip multiple spaces
+		$str = preg_replace('/ {2,}/', ' ', $str);
+		
+		// compress scales and units together so '2 hours' => '2hours'
+		$str = preg_replace('/([0-9.]+) ([cdehimnorstu]+)/', '$1$2', $str);
+		
+		foreach( explode(' ', $str) as $item ) {
+			
+			if( !preg_match('/^([0-9.]+)([cdehimnorstu]+)$/', $item, $m) )
+				return false;
+			
+			list(, $scale, $unit) = $m;
+			
+			$scale = ((float) $scale != (int) $scale) ? (float) $scale : (int) $scale;
+			
+			if( preg_match('/^h(r|our|ours)?$/', $unit) && !$hours ) {
+				$hours = $scale;
+			}
+			elseif( preg_match('/^m(in|ins|inute|inutes)?$/', $unit) && !$minutes ) {
+				$minutes = $scale;
+			}
+			elseif( preg_match('/^s(ec|ecs|econd|econds)?$/', $unit) && !$seconds ) {
+				$seconds = $scale;
+			}
+			else {
+				return false;
+			}
+			
+		}
+		
+	}
+	
+	return ($hours * 3600) + ($minutes * 60) + $seconds;
+	
+}
+
+/**
+ * Determine if an array is an associative array.
+ * Taken from: http://stackoverflow.com/questions/173400/php-arrays-a-good-way-to-check-if-an-array-is-associative-or-numeric/4254008#4254008
+ *
+ * @param  array   $var
+ * @return bool
+ */
+function is_assoc( $var ) {
+	return (bool) count(array_filter(array_keys($var), 'is_string'));
+}
+
+/**
  * Extract a single field from an array of arrays or objects.
  *
  * @param  array   $var             An array
  * @param  string  $field           The field to get values from
  * @param  boolean $preserve_keys   Whether or not to preserve the array keys
  * @return array
- *
  */
 function pluck( $var, $field, $perserve_keys = true ) {
 	$values = array();
@@ -212,6 +280,39 @@ function remove_accents( $str ) {
 }
 
 /**
+ * Converts a UTF-8 string to Latin-1 with unsupported characters encoded as numeric entities.
+ *
+ * @param  string   $str
+ * @return string   the converted string.
+ */
+function utf8_latin1( $str ) {
+	
+	/*
+	Example: I want to turn text like
+
+hello é β 水
+
+into
+
+hello é &#946; &#27700;
+	*/
+	
+	$convmap= array(0x0100, 0xFFFF, 0, 0xFFFF);
+	$encutf= mb_encode_numericentity($utf, $convmap, 'UTF-8');
+	$iso= utf8_decode($encutf);
+}
+
+/**
+ * Converts a Latin-1 string to UTF-8 and decodes entities.
+ *
+ * @param  string   $str
+ * @return string   the converted string.
+ */
+function latin1_utf8( $str ) {
+
+}
+
+/**
  * Return the ordinal suffix (st, nd, rd, th) of a number.
  * Taken from: http://stackoverflow.com/questions/3109978/php-display-number-with-ordinal-suffix
  *
@@ -244,10 +345,101 @@ function size_format( $bytes, $precision ) {
     return round($bytes, $precision). ' '. $units[$pow];
 }
 
-// strip cross-site scripting vectors
+/**
+ * Strip cross-site scripting vectors from a string.
+ * Shamelessly ripped from Kohana v2 and then tweaked to remove control characters from
+ * the regexes and replace the associated components with \s instead. Also added a couple
+ * of other tags to the really bad list.
+ * This function handles most of the XSS vectors listed at http://ha.ckers.org/xss.html
+ *
+ * @param  string   $str   the string to cleaned
+ * @return string
+ */
 function xss_clean( $str ) {
+	
+	if( !$str )
+		return $str;
+	
+	if( is_array( $str ) ) {
+		foreach( $str as &$item ) {
+			$item = $this->clean($item);
+		}
+		return $str;
+	}
+	
+	// strip any raw control characters that might interfere with our cleaning
+	$str = self::strip_control_chars($str);
+	
+	// fix and decode entities (handles missing ; terminator)
+	$str = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $str);
+	$str = preg_replace('/(&#*\w+)\s+;/', '$1;', $str);
+	$str = preg_replace('/(&#x*[0-9A-F]+);*/i', '$1;', $str);
+	$str = html_entity_decode($str, ENT_COMPAT, 'Windows-1252');
+	
+	// strip any control characters that were sneakily encoded as entities
+	$str = $this->strip_control_chars($str);
+	
+	// normalise line endings
+	$str = str_replace("\r\n", "\n", $str);
+	$str = str_replace("\r", "\n", $str);
+	
+	// remove any attribute starting with "on" or xmlns
+	$str = preg_replace('#(?:on[a-z]+|xmlns)\s*=\s*[\'"\s]?[^\'>"]*[\'"\s]?\s?#i', '', $str);
+
+	// remove javascript: and vbscript: protocols and -moz-binding CSS property
+	$str = preg_replace('#([a-z]*)\s*=\s*([`\'"]*)\s*j\s*a\s*v\s*a\s*s\s*c\s*r\s*i\s*p\s*t\s*:#i', '$1=$2nojavascript...', $str);
+	$str = preg_replace('#([a-z]*)\s*=([\'"]*)\s*v\s*b\s*s\s*c\s*r\s*i\s*p\s*t\s*:#i', '$1=$2novbscript...', $str);
+	$str = preg_replace('#([a-z]*)\s*=([\'"]*)\s*-moz-binding\s*:#', '$1=$2nomozbinding...', $str);
+
+	// only works in IE: <span style="width: expression(alert('XSS!'));"></span>
+	$str = preg_replace('#(<[^>]+?)style\s*=\s*[`\'"]*.*?expression\s*\([^>]*+>#is', '$1>', $str);
+	$str = preg_replace('#(<[^>]+?)style\s*=\s*[`\'"]*.*?behaviour\s*\([^>]*+>#is', '$1>', $str);
+	$str = preg_replace('#(<[^>]+?)style\s*=\s*[`\'"]*.*?s\s*c\s*r\s*i\s*p\s*t\s*:*[^>]*+>#is', '$1>', $str);
+
+	// remove namespaced elements (we do not need them)
+	$str = preg_replace('#</*\w+:\w[^>]*+>#i', '', $str);
+
+	// remove data URIs
+	$str = preg_replace("#data:[\w/]+;\w+,[\w\r\n+=/]*#i", "data: not allowed", $str);
+
+	// remove really unwanted tags
+	do {
+		$old = $str;
+		$str = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|body|embed|frame(?:set)?|head|html|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $str);
+	}
+	while ($old !== $str);
+
+	return $str;
+	//return mb_convert_encoding($str, 'HTML-ENTITIES', 'ISO-8859-1');
 
 }
+
+/**
+ * Remove every control character from a string except newline (10/x0A),
+ * carriage return (13/x0D), and horizontal tab (09/x09).
+ *
+ * @param  string   $str   the string to cleaned
+ * @return string
+ */
+function strip_control_chars( $str ) {
+
+	if( is_array( $str ) ) {
+		foreach( $str as &$item ) {
+			$item = $this->strip_control_chars($item);
+		}
+		return $str;
+	}
+
+	do {
+		// 00-08, 11, 12, 14-31, 127
+		$str = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $str, -1, $count);
+	}
+	while ($count);
+
+	return $str;
+
+}
+	
 
 // convert html to bbcode
 function html2bb( $str ) {
